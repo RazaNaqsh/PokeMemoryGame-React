0f028aff6ba4814c46a3cca98e65a0463fdff317
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-15 19:03:44 +0530

    Render pokemons

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,23 +1,50 @@
+import { nanoid } from "nanoid";
 import React, { useEffect, useState } from "react";
 
 const PokeCards = () => {
 	const [pokemons, setPokemons] = useState([]);
 
 	useEffect(() => {
-		// console.log(randomNum);
-		// fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}/`)
-		// 	.then((res) => res.json())
-		// 	.then((data) => setPokemons(data));
-		for (let i = 0; i < 6; i++) {
-			const randomNum = Math.ceil(Math.random() * 50);
-			fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}/`)
-				.then((res) => res.json())
-				.then((data) => setPokemons((prevData) => [...prevData, data]));
+		async function populatePokemons() {
+			for (let i = 1; i <= 5; i++) {
+				const randomNum = Math.ceil(Math.random() * (500 - 400) + 400);
+				const res = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${randomNum}/`
+				);
+				const data = await res.json();
+				if (!pokemons.includes(data)) setPokemons((prev) => [...prev, data]);
+			}
 		}
+		populatePokemons();
+		return () => {
+			setPokemons([]);
+		};
 	}, []);
 	console.log(pokemons);
 
-	return <div>PokeCards</div>;
+	const pokeElements =
+		pokemons.length === 5 ? (
+			pokemons.map((poke) => (
+				<div key={nanoid()}>
+					<h4>{poke.name}</h4>
+					<img
+						src={poke.sprites.front_default}
+						alt=""
+					/>
+				</div>
+			))
+		) : (
+			<div>Loading...</div>
+		);
+
+	return (
+		<div>
+			<h1>PokeCard</h1>
+			<div style={{ display: "flex", flexWrap: "wrap", maxWidth: "50vw" }}>
+				{pokeElements}
+			</div>
+		</div>
+	);
 };
 
 export default PokeCards;
