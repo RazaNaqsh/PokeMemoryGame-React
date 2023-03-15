053f4b7384d21dce27e6a3529f3f14a49251bf76
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-15 19:25:08 +0530

    Stores only useful data in pokemon states

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -7,12 +7,21 @@ const PokeCards = () => {
 	useEffect(() => {
 		async function populatePokemons() {
 			for (let i = 1; i <= 5; i++) {
-				const randomNum = Math.ceil(Math.random() * (500 - 400) + 400);
+				const randomNum = Math.ceil(Math.random() * (600 - 400) + 400);
 				const res = await fetch(
 					`https://pokeapi.co/api/v2/pokemon/${randomNum}/`
 				);
 				const data = await res.json();
-				if (!pokemons.includes(data)) setPokemons((prev) => [...prev, data]);
+				if (!pokemons.includes(data))
+					setPokemons((prev) => [
+						...prev,
+						{
+							id: nanoid(),
+							name: data.name,
+							src: data.sprites.front_default,
+							isClicked: false,
+						},
+					]);
 			}
 		}
 		populatePokemons();
@@ -23,12 +32,15 @@ const PokeCards = () => {
 	console.log(pokemons);
 
 	const pokeElements =
-		pokemons.length === 5 ? (
+		pokemons.length >= 5 ? (
 			pokemons.map((poke) => (
-				<div key={nanoid()}>
+				<div
+					key={nanoid()}
+					style={{ border: "1px solid grey", textAlign: "center" }}
+				>
 					<h4>{poke.name}</h4>
 					<img
-						src={poke.sprites.front_default}
+						src={poke.src}
 						alt=""
 					/>
 				</div>
@@ -40,7 +52,14 @@ const PokeCards = () => {
 	return (
 		<div>
 			<h1>PokeCard</h1>
-			<div style={{ display: "flex", flexWrap: "wrap", maxWidth: "50vw" }}>
+			<div
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					maxWidth: "50vw",
+					gap: "1rem",
+				}}
+			>
 				{pokeElements}
 			</div>
 		</div>
