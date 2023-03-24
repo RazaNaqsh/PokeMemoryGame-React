b0cb1ab70cc155f40e769d418055e84d3dd07af2
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-24 09:57:00 +0530

    Add some responsiveness using tailwind breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Game from "./components/Game";
 function App() {
 	return (
 		<div className="App">
-			<h1 className="text-center my-5 font-sans text-4xl text-[#22d3ee]">
+			<h1 className="text-center my-3 md:my-5 font-sans text-3xl md:text-4xl text-[#22d3ee]">
 				Welcome to PokeMatch
 			</h1>
 			<Game />
--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -5,20 +5,21 @@ const PokeCards = ({ pokemons, score, highScore, handleGame }) => {
 	return (
 		<>
 			<header className="flex justify-center gap-5">
-				<h2 className="text-3xl">Score: {score}</h2>
-				<h2 className="text-3xl">BestScore: {highScore}</h2>
+				<h2 className="text-2xl md:text-3xl">Score: {score}</h2>
+				<h2 className="text-2xl md:text-3xl">BestScore: {highScore}</h2>
 			</header>
-			<div className="flex flex-wrap justify-around mt-10">
+			<div className="flex flex-wrap justify-around md:mt-10 ">
 				{pokemons.map((poke) => (
 					<div
 						key={poke.id}
 						className={cardStyle}
 						onClick={() => handleGame(poke.id)}
 					>
-						<h1 className="text-[#22d3ee]">{poke.name}</h1>
+						<h1 className="text-[#22d3ee] text-sm md:text-xl">{poke.name}</h1>
 						<img
 							src={poke.imgSrc}
 							alt=""
+							className="h-[50px] w-[70px] sm:h-[80px] sm:w-[100px] object-contain md:h-auto md:w-auto"
 						/>
 					</div>
 				))}
